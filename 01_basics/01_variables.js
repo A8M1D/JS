2a9b3334b019fc4f926168d1e0e42d2ecbce6016
Kokni Ahmad <120588658+A8M1D@users.noreply.github.com>
2024-12-16 14:25:09 +0000

    variable file created

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 123456
+let accountEmail = "kokni345@gmail.com"
+var accountPassword= "1357"
+accountCity = "Maharashtra"
+
+// accountId = 2 // not allowed
+
+accountEmail = "gdjsjf@lkjlfkd.com"
+accountPassword = "64551"
+accountCity = "Goa"
+
+console.log(accountId);
+
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
