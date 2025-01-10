264c581b45d9b906152726b10b6a8a5fd4ab69cd
Ahmad Kokni <kokniahmad@gmail.com>
2025-01-10 21:40:43 +0530

    New file was added

---
+++ 01_basics/variables.js
@@ -0,0 +1,31 @@
+// var firstName = "Dea";
+// let lastName = "Bald";
+// console.log(firstName + " " + lastName);
+// const age = 30;
+
+// let favouriteFruit = "Banana";
+  
+// let course={
+//     name: "JS",
+//     hours: 25,
+//     teacher: "Dea" 
+// }
+// let property = "hours";
+// console.log( course[property] );
+
+// Functions:-
+// function sayHello(name){
+//     console.log('Hello, World! ' + name);
+// }
+
+// sayHello('Dea');
+
+// Types of Functions:-
+// function multiply(num1, num2){
+//     return num1 * num2;
+// }
+// console.log(multiply(2, 3));
+
+// Operators:-
+// Arithmetic Operators:-
+//  -,+,*,/,%
