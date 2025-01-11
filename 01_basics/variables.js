af65636d2c6542fdb6b5eb2dda11af1ad283d07e
Ahmad Kokni <kokniahmad@gmail.com>
2025-01-11 21:55:57 +0530

    New changes where made

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -29,3 +29,59 @@
 // Operators:-
 // Arithmetic Operators:-
 //  -,+,*,/,%
+
+// let programmingLanguage = "JavaScript";
+
+// // Terenary Operator:-
+// let result = programmingLanguage === "JavaScript" ? "Correct" : "Incorrect";
+// let age = 20;
+// let eligibility = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+// console.log(eligibility); // Output: Eligible to vote
+
+// let userChoosenColor = "red";
+// let defaultColor = "blue";
+
+// const currentWebsiteColor = userChoosenColor || defaultColor;
+// console.log(currentWebsiteColor); // Output: red
+
+// const course ={
+//     name: "JavaScript",
+//     duration: 6,
+//     section: 5
+// };
+
+// console.log(course['name']);
+// console.log(course.duration);
+// console.log(course.section);
+
+// for(const key in course){
+//     // console.log(key, course[key]);
+//     console.log( course[key]);
+// }
+
+// for (let i = 0; i < 10; i++){
+//     if(i === 5){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// let i =0;
+// while (i < 20){
+//     if(i===10){
+//         break;
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// let i=0;
+// do{
+//     if(i===5){
+//         i++;
+//         continue;
+//     }
+//     console.log(i);
+//     i++;
+// } while(i<10);
+
